0a405a1a5918fb3171083c63f9fc35c23d7e7264
Jianwei Xie <xiejw0217@gmail.com>
2020-06-07 15:03:27 -0700

    added one impl.

---
+++ go/snapshot/list_snapshot.go
@@ -0,0 +1,84 @@
+package snapshot
+
+type ListSnapshot struct {
+	array []*FileItem    // nil is the hole.
+	index map[string]int // Index the path to array index.
+	holes []int          // Stores the hole index in array.
+}
+
+func (sp *ListSnapshot) LookUp(fullPath string) *FileItem {
+	index, exist := sp.index[fullPath]
+	if !exist {
+		return nil
+	}
+	return sp.array[index]
+}
+
+func (sp *ListSnapshot) Add(item *FileItem) error {
+	fullPath := item.FullPath
+	_, exist := sp.index[fullPath]
+	if exist {
+		return ErrItemAlreadyExist
+	}
+
+	// First find the avaiable hole.
+	count := len(sp.holes)
+	if count != 0 {
+		holeIndex := sp.holes[count-1]
+		sp.holes = sp.holes[:count-1] // Shrink it.
+		sp.index[fullPath] = holeIndex
+		if sp.array[holeIndex] != nil {
+			panic("internal error: hole should be nil.")
+		}
+		sp.array[holeIndex] = item
+		return nil
+	}
+
+	holeIndex := len(sp.array)
+	sp.array = append(sp.array, item)
+	sp.index[fullPath] = holeIndex
+	return nil
+}
+
+func (sp *ListSnapshot) Delete(fullPath string) error {
+	deletedIndex, exist := sp.index[fullPath]
+	if !exist {
+		return ErrItemNotExist
+	}
+	delete(sp.index, fullPath)
+	sp.array[deletedIndex] = nil
+
+	// Put the index to holes
+	sp.holes = append(sp.holes, deletedIndex)
+	return nil
+}
+
+type ListSnapshotIterator struct {
+	array      []*FileItem
+	index      int
+	finalIndex int
+}
+
+func (sp *ListSnapshot) Iterator() Iterator {
+	return &ListSnapshotIterator{
+		array:      sp.array,
+		index:      0,
+		finalIndex: len(sp.array),
+	}
+}
+
+func (spi *ListSnapshotIterator) Next() *FileItem {
+	for {
+		if spi.index >= spi.finalIndex {
+			return nil
+		}
+
+		item := spi.array[spi.index]
+		spi.index++
+		if item == nil {
+			continue
+		}
+
+		return item
+	}
+}
--- go/snapshot/snapshot.go
+++ go/snapshot/snapshot.go
@@ -1,3 +1,26 @@
 package snapshot
 
-type Snapshot interface {}
+import "errors"
+
+var (
+	ErrItemNotExist     = errors.New("item not exist.")
+	ErrItemAlreadyExist = errors.New("item already exist.")
+)
+
+type FileItem struct {
+	FullPath string // Full path, but relative to base dir.
+	Checksum []byte // Allows to be empty.
+}
+
+// Order is not guaranteed.
+type Iterator interface {
+	Next() *FileItem
+}
+
+// Represents a snaptshot of the current system.
+type Snapshot interface {
+	LookUp(fullPath string) *FileItem // Returns nil for not exist.
+	Add(*FileItem) error
+	Delete(fullPath string) error
+	Iterator() Iterator
+}
