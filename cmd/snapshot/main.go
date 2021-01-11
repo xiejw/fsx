2955a0ba04e57b1aa433107ec48035a67e35890f
Jianwei Xie <xiejw0217@gmail.com>
2021-01-10 19:24:43 -0800

    added Fillregion api.;

--- cmd/snapshot/main.go
+++ cmd/snapshot/main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/xiejw/fsx/src/snapshot"
 )
 
 func main() {
-	fmt.Printf("snapshot\n")
+
+	r := snapshot.Region{
+		Name:    "testing",
+		BaseDir: ".",
+	}
+
+	err := snapshot.FillRegion(&r)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("snapshot: \n")
+	for _, item := range r.Items {
+		fmt.Printf("  %10v: %v\n", item.Size, item.RelPath)
+	}
+
 }
--- src/snapshot/snapshot.go
+++ src/snapshot/snapshot.go
@@ -1,17 +1,40 @@
 package snapshot
 
+import (
+	"github.com/xiejw/fsx/src/errors"
+)
+
 type (
 	// Represents a group of FileItems forming a file tree. Typically it could be a real file tree on
 	// the disk.
 	Region struct {
-		Name    string     // Symbol name for the region.
-		BaseDir string     // The Base dir. Root for all Items.
-		Items   []FileItem // All items in the region. Guaranteed to be sorted alphabetically.
+		Name    string      // Symbol name for the region.
+		BaseDir string      // The Base dir. Root for all Items.
+		Items   []*FileItem // All items in the region. Guaranteed to be sorted alphabetically.
 	}
 
 	FileItem struct {
-		FullPath string // Full path, but relative to Base dir.
+		RelPath  string // Full path, but relative to Base dir.
 		Size     uint64 // Length in bytes for regular files.
 		Checksum []byte // Optional sha256 checksum. See Snapshot interface for requirement.
 	}
 )
+
+func FillRegion(r *Region) error {
+	if r.BaseDir == "" {
+		return errors.New("BaseDir cannot be empty to fill region")
+	}
+	if len(r.Items) != 0 {
+		return errors.New("Items must be empty to fill region")
+	}
+
+	cb := func(re *walkResult) {
+		r.Items = append(r.Items, &FileItem{
+			RelPath:  re.Path,
+			Size:     re.Size,
+			Checksum: nil,
+		})
+	}
+
+	return walk(r.BaseDir, cb)
+}
