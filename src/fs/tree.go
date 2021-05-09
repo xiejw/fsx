ee0071a3bed70bb1d656ccac44ef10c543fb0b9d
Jianwei Xie <xiejw0217@gmail.com>
2021-05-09 15:31:25 -0700

    added test for FromLocalFS.

--- src/fs/tree.go
+++ src/fs/tree.go
@@ -5,6 +5,10 @@ import (
 	"github.com/xiejw/fsx/src/fs/scanner"
 )
 
+// -------------------------------------------------------------------------------------------------
+// public
+// -------------------------------------------------------------------------------------------------
+
 // same as clogs.FileItem
 type FileItem struct {
 	Path     string // relative to domain.
@@ -20,6 +24,14 @@ type FileTree struct {
 
 // creates a FileTree by walking the baseDir.
 func FromLocalFS(baseDir string) (*FileTree, error) {
+	return fromLocalFS(baseDir, scanner.Walk)
+}
+
+// -------------------------------------------------------------------------------------------------
+// impl
+// -------------------------------------------------------------------------------------------------
+
+func fromLocalFS(baseDir string, walkFn func(baseDir string, filters []scanner.Filter, formatter scanner.Formatter) error) (*FileTree, error) {
 	ft := &FileTree{
 		BaseDir:     baseDir,
 		HasChecksum: false, // no checksum for perf.
@@ -39,7 +51,7 @@ func FromLocalFS(baseDir string) (*FileTree, error) {
 		})
 	}
 
-	err := scanner.Walk(baseDir, []scanner.Filter{scanner.NewCommonFilter(nil)}, cb)
+	err := walkFn(baseDir, []scanner.Filter{scanner.NewCommonFilter(nil)}, cb)
 	if err != nil {
 		return nil, errors.WrapNote(err, "failed to create FileTree by walking local fs.")
 	}
--- src/fs/tree_test.go
+++ src/fs/tree_test.go
@@ -1,10 +1,13 @@
 package fs
 
 import (
+	"os"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/xiejw/fsx/src/clogs"
+	"github.com/xiejw/fsx/src/fs/scanner"
 )
 
 func TestFileItemStructSameAsCLogsFileItem(t *testing.T) {
@@ -26,4 +29,77 @@ func TestFileItemStructSameAsCLogsFileItem(t *testing.T) {
 			t.Errorf("field %v field type mismatch.", i)
 		}
 	}
+}
+
+func TestWalk(t *testing.T) {
+	walkFn := func(baseDir string, filters []scanner.Filter, formatter scanner.Formatter) error {
+		metadata := scanner.FileMetadata{
+			Path: "a",
+			Info: &newFileInfo{is_file: false},
+		}
+		formatter(metadata)
+
+		metadata.Path = "b"
+		metadata.Info = &newFileInfo{is_file: true, size: 123}
+		formatter(metadata)
+
+		metadata.Path = "c"
+		metadata.Info = &newFileInfo{is_file: true, size: 456}
+		formatter(metadata)
+
+		return nil
+	}
+
+	ft, err := fromLocalFS("abc", walkFn)
+	if err != nil {
+		t.Fatalf("unexpected err: %v", err)
+	}
+
+	if ft.BaseDir != "abc" {
+		t.Errorf("unexpected basedir: %v", err)
+	}
+
+	if ft.HasChecksum {
+		t.Errorf("unexpected checksum state.")
+	}
+
+	expected := []*FileItem{
+		{"b", 123, ""},
+		{"c", 456, ""},
+	}
+
+	if !reflect.DeepEqual(ft.Items, expected) {
+		t.Errorf("unexpected items state. expected:\n%+v\ngot:\n%+v\n", expected, ft.Items)
+	}
+}
+
+// -------------------------------------------------------------------------------------------------
+// helper
+// -------------------------------------------------------------------------------------------------
+
+type newFileInfo struct {
+	name    string
+	is_file bool
+	mode    int
+	size    int64
+}
+
+func (f *newFileInfo) Name() string {
+	return f.name
+}
+
+func (f *newFileInfo) Size() int64 {
+	return f.size
+}
+func (f *newFileInfo) Mode() os.FileMode {
+	return os.FileMode(f.mode)
+}
+func (f *newFileInfo) ModTime() time.Time {
+	return time.Now()
+}
+func (f *newFileInfo) IsDir() bool {
+	return !f.is_file
+}
+func (f *newFileInfo) Sys() interface{} {
+	return nil
 }
