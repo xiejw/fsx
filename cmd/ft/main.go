049807931fbcfc8d053c59e0047031ba11f01a7d
Jianwei Xie <xiejw0217@gmail.com>
2021-05-11 12:00:53 -0700

    started working on ft.

--- cmd/ft/main.go
+++ cmd/ft/main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/xiejw/fsx/src/fs"
 )
 
+// Handles a single domain (filetree).
 func main() {
 	fmt.Printf("Hello Ft.\n")
+
+	rootDir := "."
+
+	// Prints a local file tree.
+	ft, err := fs.FromLocalFS(rootDir)
+	if err != nil {
+		fmt.Printf("unexpected error: %v", err)
+		return
+	}
+
+	fmt.Printf("FT: %v [\n", ft.BaseDir)
+	for _, it := range ft.Items {
+		fmt.Printf("%10d %v\n", it.Size, it.Path)
+	}
+	fmt.Printf("]\n")
 }
