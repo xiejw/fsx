18a4519e5fb272633e4b721a99bc7b350c141e06
Jianwei Xie <xiejw0217@gmail.com>
2021-05-11 12:32:10 -0700

    printing total items.

--- cmd/ft/main.go
+++ cmd/ft/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/xiejw/fsx/src/errors"
 	"github.com/xiejw/fsx/src/fs"
 )
 
@@ -11,17 +12,34 @@ func main() {
 	fmt.Printf("Hello Ft.\n")
 
 	rootDir := "."
-
-	// Prints a local file tree.
-	ft, err := fs.FromLocalFS(rootDir)
+	_, err := fetchLocalFS(rootDir)
 	if err != nil {
 		fmt.Printf("unexpected error: %v", err)
 		return
 	}
+}
+
+// -------------------------------------------------------------------------------------------------
+// impl
+// -------------------------------------------------------------------------------------------------
+func fetchLocalFS(rootDir string) (*fs.FileTree, error) {
+	// Prints a local file tree.
+	ft, err := fs.FromLocalFS(rootDir)
+	if err != nil {
+		return nil, errors.WrapNote(err, "failed to fetch local file tree at: %v", rootDir)
+	}
+
+	maxItems := 10
 
 	fmt.Printf("FT: %v [\n", ft.BaseDir)
-	for _, it := range ft.Items {
+	for i, it := range ft.Items {
 		fmt.Printf("%10d %v\n", it.Size, it.Path)
+		if i == maxItems-1 && i != len(ft.Items)-1 {
+			fmt.Printf("       ... (%v items left)\n", len(ft.Items)-maxItems)
+			break
+		}
 	}
-	fmt.Printf("]\n")
+	fmt.Printf("] (%v total items)\n", len(ft.Items))
+
+	return ft, nil
 }
